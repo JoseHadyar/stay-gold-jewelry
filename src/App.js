33fdd3b7b7d03e15582be6b60d192108dec2b531
Jose Hadyar <j.hadyar@gmail.com>
2022-08-27 01:02:38 -0300

    Actualizando Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <header>
+      <div class="title">STAY GOLD</div>
+
+      <ul class="nav_bar">
+          <a href="#"><li>Productos</li></a>
+          <a href="#"><li>Promociones</li></a>
+          <a href="#"><li></li></a>
+          <a href="#"><li>Log In</li></a>
+          <a href="#"><li>Contacto</li></a>
+      </ul>
+
+
+    </header>
   );
 }
+
 
 export default App;
